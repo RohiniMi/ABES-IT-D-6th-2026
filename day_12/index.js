acf6a1b2afcec528ed9078bfce101c9e8c7cf484
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2026-04-16 11:22:38 +0530

    mongoose with express

--- day_12/index.js
+++ day_12/index.js
@@ -1,20 +1,17 @@
 import express from "express";
-import dbConnect from "./dbConnect.js";
-import User from "./models/User.js"
+import dbConnect from "./config/dbConnect.js";
+import { registerController ,getUser,changePass,deleteUser} from "./controller/crud.js";
+import { apiErrorMiddleware } from "./middleware/apiErrorMiddleware.js";
 
 const app = express();
 const PORT = 8800;
 app.use(express.json());
 dbConnect();
-app.post("/user", async (req, res) => {
-    try {
-        const userData = req.body;
-        console.log(userData);
-        
-        const user = await User.create(userData);
-        res.status(200).json({ message: "User has benn created successfully" });
-    } catch (error) {
-        res.status(500).json({ message: "Server error" });
-    }
-})
+app.get("/user/:email",getUser)
+app.post("/user",registerController);
+
+app.put("/user/:email",changePass);
+app.delete("user/:email",deleteUser);
+
+app.use(apiErrorMiddleware);
 app.listen(PORT, () => console.log(`server is running at http://localhost:${PORT}`))
